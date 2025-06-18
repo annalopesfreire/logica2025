8032f2c24952bd4fb170daec5744209321c9c596
annalopesfreire <anna.freire@unifesp.br>
2025-06-18 10:47:08 -0300

    Create  1004.c

---
+++ 1004.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, PROD;
+
+    // Lê dois valores inteiros da entrada
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    // Calcula o produto
+    PROD = A * B;
+
+    // Imprime o resultado no formato especificado
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
